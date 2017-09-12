281a0847ef1ed155350c59a0ce98e928aefd7499
Taiki IKEGAME <taikii@users.noreply.github.com>
2017-09-13 07:00:50 +0900

    New feature: 2 new options. (noheading, nosubsection)

--- README.md
+++ README.md
@@ -4,18 +4,37 @@ This plugin makes possible include wiki section.
 
 Caution!) This plugin will overwrite the default include macro.
 
+## Examples
 ```
 {{include(Foo)}}
 {{include(Foo, Bar)}} -- to include Bar section of Foo page
 ```
 
-## Installation
+### Example: Include a section.
+```
+> {{include(Child, Sec2)}}
+```
+![Example2](doc/images/example_1.png)
 
+### Example: Include a section with options.
+```
+> {{include(Child, Sec2, noheading, nosubsection)}}
+```
+![Example2](doc/images/example_2.png)
+
+### Example: Include a section with options in table.
+```
+> |_. Title |
+> | {{include(Child, Sec2, noheading, nosubsection)}} |
+```
+![Example2](doc/images/example_3.png)
+
+## Installation
 1. Clone or copy files into the Redmine plugins directory
-   `git clone https://github.com/taikii/redmine_include_macro_extension.git
+  ```
+  git clone https://github.com/taikii/redmine_include_macro_extension.git plugins/redmine_include_macro_extension
+  ```
 2. Restart Redmine
 
-
 ## License
-
 This plugin is released under the MIT License.
--- init.rb
+++ init.rb
@@ -25,17 +25,17 @@ Redmine::Plugin.register :redmine_include_macro_extension do
 
         secname = args[1].to_s
 
-        index = 0
+        regex = nil
         case Setting.text_formatting
         when "textile"
-          page.content.text.scan(/(?:\A|\r?\n\s*\r?\n)h\d+\.[ \t]+(.*?)(?=\r?\n\s*\r?\n|\z)/m).each.with_index(1) do |matched, i|
-            if matched.first.gsub(/[\r\n]/, '') == secname
-              index = i
-              break
-            end
-          end
+          regex = '(?:\A|\r?\n\s*\r?\n)h\d+\.[ \t]+(.*?)(?=\r?\n\s*\r?\n|\z)'
         when "markdown"
-          page.content.text.scan(/(?:\A|\r?\n)#+ +(.*?)(?:\r?\n|\z)/m).each.with_index(1) do |matched, i|
+          regex = '(?:\A|\r?\n)#+ +(.*?)(?=\r?\n|\z)'
+        end
+
+        index = 0
+        if regex
+          page.content.text.scan(/#{regex}/m).each.with_index(1) do |matched, i|
             if matched.first.gsub(/[\r\n]/, '') == secname
               index = i
               break
@@ -50,6 +50,20 @@ Redmine::Plugin.register :redmine_include_macro_extension do
 
         raise 'Section not found' if sectext.nil?
         raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.title) || @included_wiki_pages.include?(page.title + ':' + secname)
+
+        if args.size > 2
+          options = args[2..-1]
+
+          if options.include?("nosubsection")
+            subsecidx = sectext.index(/#{regex}/m, sectext.index(/#{regex}/m) + 1)
+            if subsecidx
+              sectext = sectext[0 .. (subsecidx - 1)]
+            end
+          end
+          if options.include?("noheading")
+            sectext.sub!(/#{regex}/m, '')
+          end
+        end
 
         @included_wiki_pages << page.title + ':' + secname
         out = textilizable(sectext, :attachments => page.attachments, :headings => false)
